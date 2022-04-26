5e79e0a993b018b0dcb206fbb5a2e4338051a586
Chris Wendler <chris.wendler@inf.ethz.ch>
2022-04-26 17:23:00 +0200

    vary also d_model

--- scripts/create_bash_commands.py
+++ scripts/create_bash_commands.py
@@ -7,11 +7,9 @@ Created on Tue Apr 26 11:31:26 2022
 """
 
 
-datasets = ["datasets/cfo/suite/bzip2d/bzip2d_1_LLVM_10_1000.csv",
-            "datasets/cfo/suite/bzip2d/no_reps_bzip2d_1_LLVM_61_1000.csv",
-            "datasets/cfo/suite/bzip2d/pow_no_reps_bzip2d_1_LLVM_61_1000.csv"]
+datasets = ["datasets/cfo/suite/bzip2d/no_reps_bzip2d_1_LLVM_61_1000.csv"]
 
-encoders = ["fc", "selfattn", "transformer"]
+encoders = ["selfattn", "transformer"]
 
 bonus_flags = ["", "--no_augmentations"]
 
@@ -29,9 +27,10 @@ for d in datasets:
         for b in bonus_flags:
             for lr in learning_rates:
                 for nl in num_layers:
-                    print("echo %d;"%i)
-                    print("bsub -o %s -G ls_krausea -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=8000]\""%(logfolder), end="")
-                    print(" -W 24:00 python project/cfo/augmented_pretrain.py --my_max_epochs 100000 --early_stopping_patience 100", end="")
-                    print(" --num_workers 12 --csv %s --lr %2.8f --num_layers %d"%(d, lr, nl), end="")
-                    print(" --wandb_project cfo_pretrain_cluster_sweep_onlyneighbors --encoder %s %s --only_neighbors;"%(e, b))
-                    i += 1
+                    for dm in d_model:
+                        print("echo %d;"%i)
+                        print("bsub -o %s -G ls_krausea -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=8000]\""%(logfolder), end="")
+                        print(" -W 24:00 python project/cfo/augmented_pretrain.py --my_max_epochs 100000 --early_stopping_patience 100", end="")
+                        print(" --num_workers 12 --csv %s --lr %2.8f --num_layers %d --d_model %d"%(d, lr, nl, dm), end="")
+                        print(" --wandb_project cfo_pretrain_cluster_sweep_onlyneighbors --encoder %s %s --only_neighbors;"%(e, b))
+                        i += 1
