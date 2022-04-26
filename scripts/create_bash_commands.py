fb41722fb2e391859ea7ef3539b7421f6bca5e9a
Chris Wendler <chris.wendler@inf.ethz.ch>
2022-04-26 16:01:41 +0200

    jobs for the cluster

--- scripts/create_bash_commands.py
+++ scripts/create_bash_commands.py
@@ -15,13 +15,19 @@ encoders = ["fc", "selfattn", "transformer"]
 
 bonus_flags = ["", "--no_augmentations"]
 
+learning_rates = [0.001, 0.0001, 0.00001]
+
+num_layers = [3, 6, 12]
+
 i = 1
 for d in datasets:
     for e in encoders:
         for b in bonus_flags:
-            print("echo %d;"%i)
-            print("bsub -G es_puesch -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=8000]\"", end="")
-            print(" -W 24:00 python project/cfo/augmented_pretrain.py --my_max_epochs 10000 --early_stopping_patience 50", end="")
-            print(" --num_workers 12 --csv %s"%(d), end="")
-            print(" --wandb_project cfo_pretrain_cluster --encoder %s %s;"%(e, b))
-            i += 1
+            for lr in learning_rates:
+                for nl in num_layers:
+                    print("echo %d;"%i)
+                    print("bsub -G ls_krausea -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=8000]\"", end="")
+                    print(" -W 24:00 python project/cfo/augmented_pretrain.py --my_max_epochs 100000 --early_stopping_patience 100", end="")
+                    print(" --num_workers 12 --csv %s --lr %2.8f --num_layers %d"%(d, lr, nl), end="")
+                    print(" --wandb_project cfo_pretrain_cluster --encoder %s %s;"%(e, b))
+                    i += 1
