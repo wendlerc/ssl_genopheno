f082dbb08a0d90da04e07dcb1b08c44571371f4d
Chris Wendler <chris.wendler@inf.ethz.ch>
2022-05-04 15:15:18 +0200

    jobs for the cluster

---
+++ scripts/genotype_pretrain.sh
@@ -0,0 +1,19 @@
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 0 --wandb_name without_augmentations_46 --seed 46
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 0 --wandb_name without_augmentations_47 --seed 47
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 0 --wandb_name without_augmentations_48 --seed 48
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 0 --wandb_name without_augmentations_49 --seed 49
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 0 --wandb_name without_augmentations_50 --seed 50
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 0 --wandb_name without_augmentations_51 --seed 51
+
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 1 --wandb_name with_augmentations_46 --seed 46
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 1 --wandb_name with_augmentations_47 --seed 47
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 1 --wandb_name with_augmentations_48 --seed 48
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 1 --wandb_name with_augmentations_49 --seed 49
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 1 --wandb_name with_augmentations_50 --seed 50
+bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch -n 12 -R "rusage[mem=1000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=8000]" -W 24:00 python project/genotype/pretrain.py --lr 0.001 --n_augs 1 --wandb_name with_augmentations_51 --seed 51
+
+
+
+
+
+
