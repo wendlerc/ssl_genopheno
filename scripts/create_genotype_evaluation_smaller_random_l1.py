55c8bbbb153fe52195b816e2b93eaeaec705402e
Chris Wendler <wendlerc@ethz.ch>
2022-05-08 18:02:06 +0200

    more scripts

---
+++ scripts/create_genotype_evaluation_smaller_random_l1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 20:48:38 2022
+
+@author: Chris Wendler
+"""
+import numpy as np
+
+barlow_name = "ridgeslowverybig_with_augmentations"
+project = "genotype_supervised_random_smallerdata_l1eq0"
+
+pretrained = ["ridgeslowverybig_with_3augmentations"]
+              #"ridgeslowverybig_with_augmentations",
+              #"ridgeslowverybig_with_3augmentations",
+              #"ridgeslowverybig_with_6augmentations",
+              #"ridgeslowverybig_without_augmentations"]
+
+for seed in (42 + np.arange(10)):
+    print("bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch", end="")
+    print(" -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=10000]\"", end="")
+    print(" -W 4:00 python project/genotype/regression.py --l1_coef 50 ", end="")
+    print(" --wandb_project %s"%project, end="")
+    print(" --use_subset --random --train_fraction 0.25", end="")
+    print(" --wandb_name trained_%d --seed %d --group trained"%(seed, seed))
+    
+    print("bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch", end="")
+    print(" -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=10000]\"", end="")
+    print(" -W 4:00 python project/genotype/regression.py --l1_coef 0.009 --freeze", end="") 
+    print(" --wandb_project %s"%project, end="")
+    print(" --use_subset --random --train_fraction 0.25", end="")
+    print(" --wandb_name random_%d --seed %d --group random"%(seed, seed))
+    
+    for pname in pretrained:
+        print("bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch", end="")
+        print(" -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=10000]\"", end="")
+        print(" -W 4:00 python project/genotype/regression.py --l1_coef 50 ", end="") 
+        print(" --wandb_pretrained %s_%d"%(pname, seed), end="")
+        print(" --wandb_project %s"%project, end="")
+        print(" --use_subset --random --train_fraction 0.25", end="")
+        print(" --wandb_name finetuned_%s_%d --seed %d --group finetuned_%s"%(pname, seed, seed, pname))
+        
+        print("bsub -o /cluster/scratch/wendlerc/lsfcsss/ -G es_puesch", end="")
+        print(" -n 12 -R \"rusage[mem=1000, ngpus_excl_p=1]\" -R \"select[gpu_mtotal0>=10000]\"", end="")
+        print(" -W 4:00 python project/genotype/regression.py --l1_coef 0.009 --freeze", end="") 
+        print(" --wandb_pretrained %s_%d"%(pname, seed), end="")
+        print(" --wandb_project %s"%project, end="")
+        print(" --use_subset --random --train_fraction 0.25", end="")
+        print(" --wandb_name frozen_%s_%d --seed %d --group frozen_%s"%(pname, seed, seed, pname))
